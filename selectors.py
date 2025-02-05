ed0e1c13ad23a977ed81193d5ccc5c46110274d0
Ignatii Fedorov <ignatii.fedorov@U-6FGX2Q4W-0121.local>
2025-02-05 20:42:49 +0300

    fix: css parser

---
+++ selectors.py
@@ -0,0 +1,26 @@
+# TagSelector for selectors like this: 'a, pre, section'
+class TagSelector:
+    def __init__(self, tag):
+        self.tag = tag
+    
+    def matches(self, node):
+        return isinstance(node, Element) and self.tag == node.tag
+
+# Combination of TagSelector's for selectors like this: 'section a, div code'
+class DescendantSelector:
+    def __init__(self, ancestor, descendant):
+        # Selector
+        self.ancestor = ancestor
+        # Selector
+        self.descendant = descendant
+    
+    def matches(self, node):
+        if not self.descendant.matches(node) return False
+
+        while node.parent:
+            if self.ancestor.matches(node.parent): return True
+            node = node.parent
+        
+        return False
+
+    
